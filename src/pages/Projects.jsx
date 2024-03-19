9d4ce733fce6fb862a7daefe962b26f2a52858f7
JOSE <jose@iMac-de-JOSE.local>
2024-03-19 18:00:30 +0100

    ejercicio terminado

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,28 @@
 //Projects.jsx
-
-//import projects from "../data/projects";
-
+//import React, { useState, useEffect } from 'react';
+import projects from '../data/projects.js';
 function Projects() {
+    function mapProjects(projects) {
+        return projects.map((project) => 
+        <div key={project}>
+            <li>
+                <h2>Name: {project.name}</h2>
+                <p>ID: {project.id}</p>
+                <img src={project.image} alt="Project Image" />
+                <p>{project.description}</p>
+                <a href={project.url}>{project.url}</a>
+            </li>
+        </div>
+        )
+    }
     return (
+        <>
         <h1>Testing la ruta de projects</h1>
+        <div>
+            {mapProjects(projects)}
+        </div>
+  
+        </>
     )
 }
 
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,48 @@
 //Resume.jsx
-
+import { studies, experiences }  from '../data/resume';
+ 
 function Resume() {
+    function mapStudies(studies) {
+        return studies.map((study) => 
+            <div key={study}>
+                <li>
+                    <h3>Title: {study.title}</h3>
+                    <p>ID: {study.id}</p>
+                    <p>Institutio: {study.institution}</p>
+                    <p>Date: {study.date}</p>
+                </li>
+            </div>
+        )
+    };
+
+    function mapExperiences(experiences) {
+        return experiences.map((experience) => 
+        <div key={experience}>
+            <li>
+                 <h3>Title: {experience.title}</h3>
+                <p>ID: {experience.id}</p>
+                <p>Company: {experience.company}</p>
+                <p>Date: {experience.date}</p>
+                
+            </li>
+
+        </div>
+        )
+    }
+
+
     return (
-        <h1>Testing la ruta de resume</h1>
+        <>
+        <h1>My resume</h1>
+        <div>
+            <h2>My studies</h2>
+            {mapStudies(studies)}
+        </div>
+        <div>
+            <h2>My experiences</h2>
+            {mapExperiences(experiences)}
+        </div>
+        </>
     )
 }
 
